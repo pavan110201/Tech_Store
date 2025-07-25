b79b4f7ab0d8a085facc3fa2e9e32c2817f88ac4
pavan110201 <vakapavankalyanreddy@gmial.com>
2025-07-25 14:32:32 -0500

    Updated Changes

--- techwarehouse/src/test/java/com/pack/techwarehouse/TechwarehouseApplicationTests.java
+++ techwarehouse/src/test/java/com/pack/techwarehouse/TechwarehouseApplicationTests.java
@@ -1,13 +1,18 @@
 package com.pack.techwarehouse;
-
+import com.pack.techwarehouse.web.LaptopController;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.beans.factory.annotation.Autowired;
 @SpringBootTest
-class TechwarehouseApplicationTests {
-
+class TechwarehouseApplicationTests 
+{
+	 @Autowired
+    private LaptopController controller;
 	@Test
-	void contextLoads() {
+	void contextLoads() 
+	{
+        assertThat(controller).isNotNull();
 	}
-
 }
+
